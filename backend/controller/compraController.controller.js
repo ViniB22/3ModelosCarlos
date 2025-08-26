acfd956e7a84b2538d6d7c0f667a42421c5fdca5
VINÍCIUS DE BONA RUAS <vinicius_ruas@estudante.sesisenai.org.br>
2025-08-26 07:46:27 -0300

    atualizar compra funcionando

--- backend/controller/compraController.controller.js
+++ backend/controller/compraController.controller.js
@@ -99,38 +99,50 @@ const atualizar = async(req,res)=>{
             if(produto === null){
                 res.status(404).json({message:'Não foi possível encontrar o id'})
             }else{
-                estoqueNovo = estoqueNovo - valores.quantidade 
-                precoSemDesconto = (quantidade * valorUnitario)
-                precoFinal = precoSemDesconto - ((precoSemDesconto * descontoAplicado) / 100)
-
-                const prodAtu = {
-                    titulo: produto.titulo,
-                    descricao: produto.descricao,
-                    categoria: produto.categoria,
-                    preco: produto.preco,
-                    porcentagemDesconto: produto.porcentagemDesconto,
-                    estoque: estoqueNovo,
-                    marca: produto.marca,
-                    imagem: produto.imagem
+                let valorUnitario = produto.preco
+                let descontoAplicado = produto.porcentagemDesconto 
+                let estoqueNovo = produto.estoque
+                
+                let quantidade = valores.quantidade
+                let precoSemDesconto = 0
+                let precoFinal = 0
+        
+                if(valores.quantidade > produto.estoque){
+                    res.status(422).json({message: "quantidade insuficiente de produto"}) // ajustar
+                }else{
+                    estoqueNovo = estoqueNovo - valores.quantidade 
+                    precoSemDesconto = (quantidade * valorUnitario)
+                    precoFinal = precoSemDesconto - ((precoSemDesconto * descontoAplicado) / 100)
+                    
+                    const prodAtu = {
+                        titulo: produto.titulo,
+                        descricao: produto.descricao,
+                        categoria: produto.categoria,
+                        preco: produto.preco,
+                        porcentagemDesconto: produto.porcentagemDesconto,
+                        estoque: estoqueNovo,
+                        marca: produto.marca,
+                        imagem: produto.imagem
+                    }
+                
+                    await Produto.update(prodAtu,{where:{id:produto.id}})
+                
+                    const compraVal = {
+                        fk_produto: valores.fk_produto,
+                        fk_usuario: valores.fk_usuario,
+                        quantidade: valores.quantidade,
+                        dataCompra: valores.dataCompra,
+                        valorUnitario: produto.preco,
+                        descontoAplicado: produto.porcentagemDesconto,
+                        precoFinal: precoFinal,
+                        tipoPagamento: valores.tipoPagamento,
+                        status: valores.status
+                    }
+                
+                    await Compra.update(compraVal,{where:{id:id}})
+                    dados = await Compra.findByPk(id)
+                    res.status(200).json(dados)
                 }
-
-                await Produto.update(prodAtu,{where:{id:produto.id}})
-
-                const compraVal = {
-                    fk_produto: valores.fk_produto,
-                    fk_usuario: valores.fk_usuario,
-                    quantidade: valores.quantidade,
-                    dataCompra: valores.dataCompra,
-                    valorUnitario: produto.preco,
-                    descontoAplicado: produto.porcentagemDesconto,
-                    precoFinal: precoFinal,
-                    tipoPagamento: valores.tipoPagamento,
-                    status: valores.status
-                }
-
-                await Compra.update(compraVal,{where:{id:id}})
-                dados = await Compra.findByPk(id)
-                res.status(200).json(dados)
             }
         }else{
             res.status(404).json({message:'Não foi possível encontrar o id'})
